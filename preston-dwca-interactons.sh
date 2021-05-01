0ca68090b4f9525287cc0867e1a29470955006d7
jose <zedomel@gmail.com>
2021-04-30 23:57:50 -0300

    Some changes to save dwca versions and bug fixes

--- preston-dwca-interactons.sh
+++ preston-dwca-interactons.sh
@@ -2,20 +2,18 @@
 #
 #
 
-#QUERY_HASH=$1
-QUERY_HASH=c253a5311a20c2fc082bf9bac87a1ec5eb6e4e51ff936e7be20c29c8e77dee55
-DWCA_URL_FILE=dwca_urls.txt
 DATASET_DIR=./datasets
 
-preston ls --remote https://deeplinker.bio/$QUERY_HASH --log tsv --no-cache | grep 'application/dwca' | cut -f1 > $DWCA_URL_FILE
+# Get last graph version
+QUERY_HASH=`preston history | grep 'hasVersion' | grep -oE "hash://sha256/[a-f0-9]{64}"`
 
 # get the provenance log
-preston cat hash://sha256/$QUERY_HASH --remote https://deeplinker.bio\
+preston cat $QUERY_HASH --remote https://deeplinker.bio\
 | grep 'application/dwca'\
 | cut -d ' ' -f1,3\
 | sort | uniq > dwca.txt
 
-preston cat hash://sha256/$QUERY_HASH --remote https://deeplinker.bio\
+preston cat $QUERY_HASH --remote https://deeplinker.bio\
 | grep 'http://purl.org/pav/hasVersion'\
 | cut -d ' ' -f1,3\
 | sort | uniq > versions.txt
@@ -28,7 +26,7 @@ join <(sort versions.txt) <(sort dwca.txt)\
 while read -r dwcaHash;
 do
         preston cat "$dwcaHash" --remote https://deeplinker.bio > dwca.zip
-        
+
         jq -n --arg format dwca --arg citation "$dwcaHash" '{"format":"dwca","citation":$citation,"url":"dwca.zip"}' > globi.json
 
         # Search for interactions
@@ -38,5 +36,5 @@ do
         elton review --type summary >> reviews.txt
 
         # do cleanup here if needed
-        rm -r datasets globi.json dwca.zip
-done < $DWCA_URL_FILE
+        rm -rf datasets globi.json dwca.zip
+done < dwca-versions.txt
