4f5215736e56a288f6d852e81aa2712c8394bbe3
jose <zedomel@gmail.com>
2021-05-03 11:56:40 -0300

    Added option to output to a specified directory

--- preston-dwca-interactons.sh
+++ preston-dwca-interactons.sh
@@ -2,42 +2,45 @@
 #
 #
 
+CACHE_DIR=${1:-"./"}
+echo "Output to ${CACHE_DIR}"
+
 # Get last graph version
-QUERY_HASH=`preston history --remote https://deeplinker.bio\
+QUERY_HASH=`preston history --data-dir $CACHE_DIR/biodata --remote https://deeplinker.bio\
 | grep 'hasVersion'\
 | grep -oE "hash://sha256/[a-f0-9]{64}"`
 echo $QUERY_HASH
 
 # get the provenance log
-preston cat $QUERY_HASH --remote https://deeplinker.bio\
+preston cat $QUERY_HASH --data-dir $CACHE_DIR/biodata --remote https://deeplinker.bio\
 | grep 'application/dwca'\
 | cut -d ' ' -f1,3\
-| sort | uniq > dwca.txt
+| sort | uniq > $CACHE_DIR/dwca.txt
 
-preston cat $QUERY_HASH --remote https://deeplinker.bio\
+preston cat $QUERY_HASH --data-dir $CACHE_DIR/biodata --remote https://deeplinker.bio\
 | grep 'http://purl.org/pav/hasVersion'\
 | cut -d ' ' -f1,3\
-| sort | uniq > versions.txt
+| sort | uniq > $CACHE_DIR/versions.txt
 
 # create a list of unique dwca versions, identified by their sha256 hashes
 join <(sort versions.txt) <(sort dwca.txt)\
 | grep -o -E "hash://sha256/[a-f0-9]{64}"\
-| sort | uniq > dwca-versions.txt
+| sort | uniq > $CACHE_DIR/dwca-versions.txt
 
 while read -r dwcaHash;
 do
         echo $dwcaHash
-        preston cat "$dwcaHash" --remote https://deeplinker.bio > dwca.zip
+        preston cat "$dwcaHash" --data-dir $CACHE_DIR/biodata --remote https://deeplinker.bio > dwca.zip
 
         jq -n --arg format dwca --arg citation "$dwcaHash" '{"format":"dwca","citation":$citation,"url":"dwca.zip"}' > globi.json
 
         # Search for interactions
-        elton interactions >> interactions.tsv 2> /dev/null
+        elton interactions >> $CACHE_DIR/interactions.tsv 2> /dev/null
 
         # Save review summary
-        elton review --type summary >> reviews.txt 2> /dev/null
+        elton review --type summary >> $CACHE_DIR/reviews.txt 2> /dev/null
 
         # do cleanup here if needed
         rm -rf datasets globi.json dwca.zip
-done < dwca-versions.txt
+done < $CACHE_DIR/dwca-versions.txt
 
