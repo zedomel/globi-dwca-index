94eb2f30ed265b603b3fc8f48f5771ec8499c6c0
José Augusto Salim <jasalim@unicamp.br>
2022-10-03 15:33:25 -0300

    Remove duplicate and ambiguous interaction types

--- conf/update.sh
+++ conf/update.sh
@@ -3,20 +3,22 @@
 # Retrieves and updates interaction translation tables
 #
 
-
 function get_mapping {
-  curl -L "https://docs.google.com/spreadsheets/u/0/d/1AxCSJYA5dGSDZKDR-GLRglL-xQQP_Se54J2vpjHGgkQ/export?format=csv"
+  curl -L "https://docs.google.com/spreadsheets/u/0/d/1gNVEiN5GJxrei6QwgqC_sbvKzaJrktm_Pv1Psqx2GtI/export?format=csv"
 }
 
 
 echo "provided_interaction_type_label,provided_interaction_type_id,mapped_to_interaction_type_label,mapped_to_interaction_type_id"\
-  > interaction_types_mappings.csv
+  > interaction_types_mapping.csv
 
 get_mapping\
  | grep -v ignore\
  | grep "purl"\
  | cut -d ',' -f1-4\
- >>  interaction_types_mappings.csv
+ | awk -F, '{print tolower($1)","$2","$3","$4}'\
+ | sort -k 1 -t,\
+ | uniq\
+ >>  interaction_types_mapping.csv
 
 echo "interaction_type_ignored"\
  > interaction_types_ignored.csv
@@ -24,4 +26,6 @@ echo "interaction_type_ignored"\
 get_mapping\
  | grep ignore\
  | cut -d ',' -f1\
+ | sort\
+ | uniq\
  >> interaction_types_ignored.csv
